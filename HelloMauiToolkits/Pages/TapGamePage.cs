375723be74da7a7fd63e37ba853826654fe7df4a
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-10-08 16:36:04 +0200

    Organize Folder Structure

--- HelloMauiToolkits/Pages/TapGamePage.cs
+++ HelloMauiToolkits/Pages/TapGamePage.cs
@@ -4,6 +4,10 @@ class TapGamePage : BasePage<TapGameViewModel>
 {
 	public TapGamePage(TapGameViewModel viewModel) : base(viewModel)
 	{
+		Title = "Tap Game";
 		
+		Content = new 
 	}
+	
+	enum Row { HighScore, Description, TapButton, TapCounter }
 }
---
+++ HelloMauiToolkits/Services/TapCountService.cs
@@ -0,0 +1,9 @@
+namespace HelloMauiToolkits.Services;
+
+class TapCountService(IPreferences preferences)
+{
+	public int TapCountHighScore
+	{
+		get => preferences.
+	}
+}
